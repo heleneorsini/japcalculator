f1339465a2e7d5afedf0ecac8a5fe6f6adb7f40a
Orsini Helene <helene.orsini@supelec.fr>
2020-11-10 11:29:50 +0100

    add picture

--- src/data/food.ts
+++ src/data/food.ts
@@ -4,7 +4,7 @@ import Maki from "../assets/images/food/Maki.svg";
 import Sushi from "../assets/images/food/Sushi.svg";
 import California from "../assets/images/food/California.svg";
 import BeefCheese from "../assets/images/food/Brochette boeuf fro.svg";
-import Sashimi from "../assets/images/food/Sushi.svg";
+import Sashimi from "../assets/images/food/Sashimi.svg";
 
 export interface FoodItem {
   name: string;
